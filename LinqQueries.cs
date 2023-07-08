3e546ce4fd9fa212903cd9fd87861fae181cd4b6
torvic <torvic02st96@gmail.com>
2023-07-07 21:40:28 -0500

    importing data

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -1,7 +1,18 @@
 public class LinqQueries
 {
+	private List<Book> booksList = new List<Book>();
+
 	public LinqQueries()
 	{
+		using (StreamReader reader = new StreamReader("books.json"))
+		{
+			string json = reader.ReadToEnd();
+			this.booksList = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+		}
+	}
 
+	public IEnumerable<Book> getAllBooks()
+	{
+		return booksList;
 	}
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿LinqQueries queries = new LinqQueries();
+
+printValues(queries.getAllBooks());
+
+void printValues(IEnumerable<Book> booksList)
+{
+	Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+	foreach (var book in booksList)
+	{
+		Console.WriteLine("{0,-60} {1, 15} {2, 15}", book.Title, book.PageCount, book.PublishedDate.ToShortDateString());
+	}
+}
